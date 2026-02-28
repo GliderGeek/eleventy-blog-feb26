4827da6cbfbb1a19240ed9d15d21448e0fddeb5f
glidergeek <GliderGeek@users.noreply.github.com>
2026-02-28 17:47:12 +0100

    build site for gh pages

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	url: "https://GliderGeek.github.io/eleventy-blog-feb26/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Your Name Here",
 		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		url: "https://GliderGeek.github.io/eleventy-blog-feb26/about/"
 	}
 }
